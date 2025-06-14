dc772292f0a1c9d0ee761f2a7c55f7746dddb0bd
Katyrguez <rkatherine538@gmail.com>
2025-06-14 11:33:17 -0400

    functions remove_spaces, remove_special_characters

--- src/preprocessing/gutenberg.py
+++ src/preprocessing/gutenberg.py
@@ -3,3 +3,9 @@ import unicodedata
 
 def normalize_unicode(text):
     return unicodedata.normalize("NFKD", text).encode("ascii", "ignore").decode("utf-8")
+
+def remove_spaces(text):
+    return re.sub(r"\s+", " ", text).strip()
+
+def remove_special_characters(text):
+    return re.sub(r"[^\w\s,.:\-]", "", text)
