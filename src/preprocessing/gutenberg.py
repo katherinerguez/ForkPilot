f63ca4d904bbb9c428942b34733fd4133d3fd937
Katyrguez <rkatherine538@gmail.com>
2025-06-14 11:34:23 -0400

    functions to remove headers and footers

--- src/preprocessing/gutenberg.py
+++ src/preprocessing/gutenberg.py
@@ -9,3 +9,13 @@ def remove_spaces(text):
 
 def remove_special_characters(text):
     return re.sub(r"[^\w\s,.:\-]", "", text)
+
+def remove_headers(text):
+    text=text.lower()
+    for i in [r'.*?preface\s*', r'.*?table of contents\s*', r'.*?contents\s*']:
+        text = re.sub(i, '', text, 1)
+    return text
+
+def remove_footer(text):
+    text=re.sub(r'\bsection\s*\d+', '', text)
+    return re.sub(r'\bgeneral information about project gutenberg\b.*', '', text)
