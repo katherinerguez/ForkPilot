881af2880e6e932fc6419ca40cf9bdda1e6ed884
Katyrguez <rkatherine538@gmail.com>
2025-06-16 02:26:50 -0400

    perform semantic chuncking

--- src/preprocessing/gutenberg.py
+++ src/preprocessing/gutenberg.py
@@ -1,5 +1,7 @@
 import re
 import unicodedata
+from langchain_experimental.text_splitter import SemanticChunker
+from langchain_community.embeddings import HuggingFaceEmbeddings
 
 def normalize_unicode(text):
     return unicodedata.normalize("NFKD", text).encode("ascii", "ignore").decode("utf-8")
@@ -21,10 +23,31 @@ def remove_footer(text):
     text=re.sub(r'\bend of the project gutenberg ebook\b.*', '', text)
     return re.sub(r'\bstart: full license\b.*', '', text)
 
+def split_text_semantically(text, breakpoint_type="percentile"):
+    # Usar embeddings locales gratuitos
+    embeddings = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2"
+    )
+    
+    text_splitter = SemanticChunker(embeddings, breakpoint_threshold_type=breakpoint_type)
+    docs = text_splitter.create_documents([text])
+    return [doc.page_content for doc in docs]
+
+def main(document_content):
+ 
+    # Dividir el texto utilizando el tipo de umbral elegido (percentil)
+    tipo_umbral = "percentile"
+    fragments = split_text_semantically(document_content, breakpoint_type=tipo_umbral)
+    chuncks=[]
+    for i, fragment in enumerate(fragments):
+        chuncks.append(fragment)
+    return chuncks
+
 def clean_text(text):
     text = normalize_unicode(text)
     text = remove_headers(text)         
     text = remove_footer(text)
     text = remove_special_characters(text)
     text = remove_spaces(text)
+    text= main(text)
     return text
