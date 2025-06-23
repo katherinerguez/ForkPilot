b14d79738e32e08cfc3f687a7f50af307c1b1fd7
mariandc18 <ma907222@gmail.com>
2025-06-23 03:39:25 -0400

    add docstrings to preprocessing functions

--- src/preprocessing/gourmet.py
+++ src/preprocessing/gourmet.py
@@ -2,9 +2,15 @@ import re
 import unicodedata
 
 def normalize_unicode(text):
+    """
+    Normaliza el texto 
+    """
     return unicodedata.normalize("NFKD", text).encode("ascii", "ignore").decode("utf-8")
 
 def remove_social_links(text):
+    """
+    Elimina links de redes sociales
+    """
     patterns = [
         r"https?://[^\s]+",
         r"@gourmetjournal"
@@ -14,19 +20,34 @@ def remove_social_links(text):
     return text
 
 def remove_extra_whitespace(text):
+    """
+    Elimina espacios en blanco extra
+    """
     return re.sub(r"\s+", " ", text).strip()
 
 def remove_special_characters(text):
+    """
+    Elimina caracteres especiales
+    """
     return re.sub(r"[^\w\s,.:\-]", "", text)
 
 def clean_promotions(text):
+    """
+    Elimina promociones
+    """
     return re.sub(r"(P\.D\.|Descubre más en|Síguenos en|Visita nuestro sitio web|Ver:|).*", "", text, flags=re.IGNORECASE)
 
 def restructure_text(text):
+    """
+    Restaura el texto
+    """
     text = re.sub(r"\n\s*\n", "\n", text) 
     return text
 
 def cleaned_text(text):
+    """
+    Aplica todas las funciones anteriores
+    """
     text = clean_promotions(text)
     text = normalize_unicode(text)
     text = remove_social_links(text)
